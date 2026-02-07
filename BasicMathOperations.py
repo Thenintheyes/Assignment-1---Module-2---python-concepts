41c543a0698c9c4e2b888293ffdf1a21e7d2b6ed
Thenintheyes <102641421+Thenintheyes@users.noreply.github.com>
2026-02-07 17:39:30 +0530

    Add files via upload

---
+++ BasicMathOperations.py
@@ -0,0 +1,12 @@
+a = int(input("Enter number a :"))
+b = int(input("Enter number b :"))
+
+Addition = a+b
+Subtraction = a-b
+Multiplication = a*b
+Division = a/b
+
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
